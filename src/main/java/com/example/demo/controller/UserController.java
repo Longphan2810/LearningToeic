c582843728d0c5fe2091b8e3422373c9671d5234
hieumm2608 <hieu0291dn@gmail.com>
2024-06-28 01:01:26 +0700

    cap nhat cau hinh jsp

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/page1")
+	public String requestMethodName() {
+		return "User/Account";
+	}
+	
+	
+}
