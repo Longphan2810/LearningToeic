b5ab91167fc0bd4947e8c680d3ec600c06015e99
hieumm2608 <hieu0291dn@gmail.com>
2024-06-30 21:14:06 +0700

    them jsp questionT2

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,50 +1,61 @@
 package com.example.demo.controller;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class UserController {
-	
+
 	@RequestMapping("/page1")
 	public String requestMethodName() {
 		return "User/Account";
 	}
-	
+
 	@RequestMapping("/layout")
 	public String requestLayout() {
 		return "User/layout";
 	}
-	
 
 	@RequestMapping("/Question")
 	public String ques() {
 		return "User/QuestionType1";
 	}
-	
+
+	@RequestMapping("/Question2")
+	public String ques2(Model model) {
+		String input = "Hello";
+		String[] output = input.toUpperCase().split("");
+		model.addAttribute("output",output);
+		for (String string : output) {
+			System.out.println(string);
+		}
+		return "User/QuestionType2";
+	}
 
 	@RequestMapping("/voca")
 	public String requestVoca() {
 		return "User/Vocabulary";
 	}
-	
+
 	@RequestMapping("/info")
 	public String requestInfo() {
 		return "User/info";
 	}
-	
+
 	@RequestMapping("/home")
 	public String requestHome() {
 		return "User/home2";
 	}
-	
+
 	@RequestMapping("/account")
 	public String requestaccount() {
 		return "User/Account";
 	}
 
-	
 }
